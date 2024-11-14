503e1571625375308914cd2f5cc845d571d243d5
NicoOhR <nimoohayonr@gmail.com>
2024-11-14 15:08:49 -0600

    read I2C

--- main/dlhr_driver.c
+++ main/dlhr_driver.c
@@ -1,4 +1,6 @@
 #include "driver/i2c_master.h"
+#include "driver/i2c_types.h"
+#include "esp_err.h"
 #include "sdkconfig.h"
 #include <stdio.h>
 
@@ -16,4 +18,23 @@ void app_main(void) {
       .glitch_ignore_cnt = 7,
       .flags.enable_internal_pullup = true,
   };
+
+  i2c_master_bus_handle_t bus_handle;
+
+  ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_mst_config, &bus_handle));
+
+  i2c_device_config_t dev_cfg = {
+      .dev_addr_length = I2C_ADDR_BIT_LEN_7,
+      .device_address = 0x58,
+      .scl_speed_hz = 100000,
+  };
+
+  i2c_master_dev_handle_t dev_handle;
+  ESP_ERROR_CHECK(i2c_master_bus_add_device(bus_handle, &dev_cfg, &dev_handle));
+
+  const uint8_t command_buffer[1] = {0xAA};
+  uint8_t recieve_buffer[8] = {0};
+
+  ESP_ERROR_CHECK(i2c_master_transmit_receive(dev_handle, command_buffer, 1,
+                                              recieve_buffer, 8, -1));
 }
